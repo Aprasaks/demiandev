2c29ac39cc3841d82f3e70ea3eefcd11b4445cb0
Y.KangHyeok <heavenin24@naver.com>
2025-05-22 11:55:03 +0900

    dynamic 수정

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,8 +1,5 @@
 // src/app/write/page.jsx
-import dynamic from "next/dynamic";
-
-// 클라이언트 컴포넌트로 WriteEditor 불러오기 (SSR X)
-const WriteEditor = dynamic(() => import('@/components/WriteEditor'), { ssr: false });
+import WriteEditor from '@/components/WriteEditor';
 
 export default function WritePage() {
   return <WriteEditor />;
